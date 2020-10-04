fde0b803aed9f982fa5f63bf071ed9852bed98c2
zinirun <heojj97@naver.com>
2020-10-04 18:08:13 +0900

    Add ~ Express

--- Express/server.js
+++ Express/server.js
@@ -1,54 +1,6 @@
-'use strict';
+const app = require('./app.js');
+const port = 3000;
 
-const express = require('express');
-const http = require('http');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const static = require('serve-static');
-
-class ApiServer extends http.Server {
-    constructor(config) {
-        const app = express();
-        super(app);
-        this.config = config;
-        this.app = app;
-        this.currentConnects = new Set();
-        this.busy = new WeakSet();
-        this.stopping = false;
-        this.app.static = static;
-    }
-
-    // Set Middleware
-    async start() {
-        this.app.use(helmet());
-        this.app.use(cookieParser());
-        this.app.use(bodyParser());
-        this.app.use((err, req, res, next) => {
-            console.error(`Internal Error: ${err}`);
-            if (req) {
-                console.log(req);
-            }
-            if (res) {
-                console.log(res);
-            }
-            next();
-        });
-
-        // 정적 파일 처리
-        this.app.use(
-            this.app.static(path.join(__dirname, 'dist'), {
-                setHeaders: (res, path) => {
-                    res.setHeaders('Access-Control-Allow-Origin', '*');
-                    res.setHeaders('Access-Control-Allow-Headers', '*');
-                    res.setHeaders('Access-Control-Allow-Method', 'GET');
-                },
-            }),
-        );
-    }
-}
-
-const init = async (config = {}) => {
-    const server = new ApiServer(config);
-    return server.start();
-};
+app.listen( port, function(){
+    console.log('Express listening on port', port);
+});
